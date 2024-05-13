dc829a2fb800ad1a064e9cda3a7072896abbd1fd
ElegantDev43 <elegantdeveloper43@gmail.com>
2024-05-13 23:15:50 +0900

    Revert "Frontend"

--- src/telegram/settings/chains.py
+++ src/telegram/settings/chains.py
@@ -3,11 +3,9 @@ from telebot import types
 import config
 from src.database import user as user_model
 
-
 def handle_chains(bot, message):
-    # user = user_model.get_user_by_telegram(message.chat.id)
-    chains = ['ethereum', 'solana', 'base']
-    current_chain = 'ethereum'
+    user = user_model.get_user_by_telegram(message.chat.id)
+    current_chain = user.chain
 
     text = f'''
 *Settings > Chains*
@@ -18,7 +16,7 @@ Select the chain you'd like to use. You can only have one chain selected at the 
     '''
 
     buttons = []
-    for chain in chains:
+    for chain in config.CHAINS:
         caption = f'✅ {chain}' if chain == current_chain else chain
         button = types.InlineKeyboardButton(text=caption, callback_data=chain)
         buttons.append(button)
@@ -29,9 +27,7 @@ Select the chain you'd like to use. You can only have one chain selected at the 
         keyboard.row(button)
     keyboard.row(back)
 
-    bot.send_message(chat_id=message.chat.id, text=text,
-                     parse_mode='Markdown', reply_markup=keyboard)
-
+    bot.send_message(chat_id=message.chat.id, text=text, parse_mode='Markdown', reply_markup=keyboard)
 
 def handle_select_chain(bot, message, next_chain):
     user = user_model.get_user_by_telegram(message.chat.id)
@@ -59,7 +55,5 @@ Select the chain you'd like to use. You can only have one chain selected at the 
         keyboard.row(button)
     keyboard.row(back)
 
-    bot.edit_message_text(chat_id=message.chat.id,
-                          message_id=message.message_id, text=text, parse_mode='Markdown')
-    bot.edit_message_reply_markup(
-        chat_id=message.chat.id, message_id=message.message_id, reply_markup=keyboard)
+    bot.edit_message_text(chat_id=message.chat.id, message_id=message.message_id, text=text, parse_mode='Markdown')
+    bot.edit_message_reply_markup(chat_id=message.chat.id, message_id=message.message_id, reply_markup=keyboard)
