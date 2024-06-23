588ecdfff516da995010587ad270ad9b437dd0ba
ElegantDev43 <elegantdeveloper43@gmail.com>
2024-06-23 10:47:33 +0800

    Add the new pool detection from Raydium using WebSocket

--- src/engine/main.py
+++ src/engine/main.py
@@ -1,12 +1,9 @@
 from threading import Thread
 
-from src.engine import lp_sniper
-
+from src.engine import raydium
 from src.engine.swing import main as swing_engine
 
 def initialize():
   print('Initialize Engine')
-  
   #swing_engine.initialize()
-  
-  #Thread(target=lp_sniper.initialize).start()
+  Thread(target=raydium.initialize).start()
