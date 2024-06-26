312fa3451b177fd27a45cac8bc692fbb4b7d7d73
ElegantDev43 <elegantdeveloper43@gmail.com>
2024-06-26 23:30:44 +0800

    Update token library

--- src/engine/chain/solana/token.py
+++ src/engine/chain/solana/token.py
@@ -2,6 +2,8 @@ import requests
 import json
 from solana.rpc.api import Client
 from solders.pubkey import Pubkey # type: ignore
+from solders.keypair import Keypair # type: ignore
+from solana_tracker.solanatracker import SolanaTracker
 import base58
 import re
 
@@ -21,7 +23,7 @@ def is_valid(token):
 
   try:
     account_info = client.get_account_info(Pubkey.from_string(token)).value.to_json()
-    owner = json.loads(account_info)['owner']
+    owner = json.loads(account_info).get('owner', '0')
     token_program_id = "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA"
     if str(owner) == token_program_id:
       return True
@@ -31,69 +33,107 @@ def is_valid(token):
     return False
 
 def get_metadata(token):
-  url = "https://mainnet.helius-rpc.com/?api-key=c05d1904-280f-42c5-affc-a66bd9247093"
-  payload = {
-    "jsonrpc": "2.0",
-    "id": "id",
-    "method": "getAsset",
-    "params": {
-      "id": token
+    url = "https://mainnet.helius-rpc.com/?api-key=c05d1904-280f-42c5-affc-a66bd9247093"
+    payload = {
+        "jsonrpc": "2.0",
+        "id": "id",
+        "method": "getAsset",
+        "params": {
+            "id": token
+        }
     }
-  }
 
-  headers = {
-    "Content-Type": "application/json"
-  }
+    headers = {
+        "Content-Type": "application/json"
+    }
 
-  response = requests.post(url, headers=headers, data=json.dumps(payload))
-  result = response.json()['result']
+    try:
+        response = requests.post(url, headers=headers, data=json.dumps(payload))
+        response.raise_for_status()  # Raises an HTTPError for bad responses (4xx or 5xx)
+        result = response.json().get('result')
 
-  name = result['content']['metadata']['name']
-  symbol = result['content']['metadata']['symbol']
-  decimals = result['token_info']['decimals']
+        if not result:
+            return 0
 
-  return {
-    'name': name,
-    'symbol': symbol,
-    'decimals': int(decimals)
-  }
+        content = result.get('content')
+        if not content:
+            return 0
+
+        metadata = content.get('metadata')
+        if not metadata:
+            return 0
+
+        token_info = result.get('token_info')
+        if not token_info:
+            return 0
+
+        name = metadata.get('name', 0)
+        symbol = metadata.get('symbol', 0)
+        decimals = token_info.get('decimals', 0)
+
+
+        return {
+            'name': name,
+            'symbol': symbol,
+            'decimals': int(decimals)
+        }
+    except requests.RequestException as e:
+        print(f"Failed to fetch metadata. Exception: {e}")
+        return 0
+    except (ValueError, KeyError, TypeError) as e:
+        print(f"Error processing the response. Exception: {e}")
+        return 0
 
 def check_liveness(token):
-  if get_jupiter_price(token) > 0:
-    return 'jupiter'
-  elif get_raydium_price(token) > 0:
-    return 'raydium'
-  
-  PUMPFUN_METADATA_API = "https://pumpportal.fun/api/data/token-info?ca="
-  response = requests.get(PUMPFUN_METADATA_API + token)
-  if response.status_code == 200:
-    data = response.json()
-    if data.get('data'):
-      return 'pumpfun'
-    else:
+  keypair = Keypair()
+  solana_tracker = SolanaTracker(keypair, "https://rpc.solanatracker.io/public?advancedTx=true")
+
+  try:
+      swap_response = solana_tracker.get_swap_instructions(
+          "So11111111111111111111111111111111111111112",  # From Token
+          token,  # To Token
+          0.0001,  # Amount to swap
+          10,  # Slippage
+          str(keypair.pubkey()),  # Payer public key
+          0.00005,  # Priority fee (Recommended while network is congested)
+          True  # Force legacy transaction for Jupiter
+      )
+
+      # Check the response for the specific error message
+      if swap_response.get('message') == 'To token address is invalid.':
+          return False
+      else:
+          return True
+
+  except Exception as e:
+      # Handle exceptions that indicate the swap failed
       return False
+  
 
 def get_market_data(token):
-  api_url = f"https://gmgn.ai/defi/quotation/v1/tokens/sol/{token}"
+    api_url = f"https://gmgn.ai/defi/quotation/v1/tokens/sol/{token}"
 
-  response = requests.get(api_url)
+    try:
+        response = requests.get(api_url)
+        response.raise_for_status()  # Raises an HTTPError if the response code was unsuccessful
+        data = response.json()
 
-  if response.status_code == 200:
-    data = response.json()
-    if data['msg'] == "invalid argument: invalid token address":
-      return 999
-    elif not data['data']['token']:
-      return 999
-    else:
-      token_price = data['data']['token']['price']
-      liquidity = data['data']['token']['liquidity']
-      market_cap = data['data']['token'].get('market_cap', '1')
-      # print(f"Token Price: {token_price:.18f}")
-      # print(f"Liquidity: {liquidity}")
-      # print(f"Market Cap: {market_cap}")
-      return {'price':token_price, 'liquidity':liquidity, 'market_capital':int(market_cap)}
-  else:
-    print(f"Failed to fetch data. Status code: {response.status_code}")
+        if data['msg'] == "invalid argument: invalid token address":
+            return 999
+        token_data = data.get('data')
+        if not token_data:
+            return 999
+        token_info = token_data.get('token')
+        if not token_info:
+            return 999
+
+        token_price = token_info.get('price', 0)
+        liquidity = token_info.get('liquidity', 0)
+        market_cap = token_info.get('market_cap', '1')
+
+        return {'price': token_price, 'liquidity': liquidity, 'market_capital': int(market_cap)}
+    except requests.RequestException as e:
+        return 0
 
 def get_jupiter_price(token):
   try:
